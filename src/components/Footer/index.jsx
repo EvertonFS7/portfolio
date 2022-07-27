86c541f69aa9006cb459d189282430a19afc5b12
Everton Fernandes <evertonfernandes.dev@gmail.com>
2022-07-27 16:28:47 -0300

    footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+
+const Footer = () => {
+    return (
+        <footer className="container">
+            <div className="footer-content">
+                <p className="footer-description">Email: evertonfernandes.dev@gmail.com </p>
+                <p className="footer-made">&copy; feito por Everton Fernandes</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
